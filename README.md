a5f55e4553f098d9a61b43e2e36b1fc613e58724
w1109 <a18277278902@gmail.com>
2019-02-07 20:23:13 +0800

    ggn

---
+++ README.md
@@ -0,0 +1,6 @@
+# .DS_Store banished!
+```shell
+echo .DS_Store >> .gitignore
+```
+# pycharm、python3
+# 完成ggn2000种子下载任务
---
+++ ggnseed.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+__author__ = 'admin'
+
+
+def fun():
+    print()
+    print()
+
+
+if __name__ == '__main__':
+    print("Hello Python")
